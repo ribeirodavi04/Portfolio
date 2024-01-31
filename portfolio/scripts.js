7fe3a2731db2f35071a6c304333529a25266632c
ribeirodavi04 <ribeirodavi04@outlook.com>
2024-01-30 23:54:47 -0300

    feat:adiciona navbar e seção sobre mim

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -1,14 +1,22 @@
 (function ($) {
-    "use strict"
+  "use strict";
 
-    if($('.typed-text-output').length == 1){
-        var typed_strings = $('.typed-text').text();
-        var typed = new Typed('.typed-text-output', {
-            strings:typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        })
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".navbar").addClass("navbar-light bg-light shadow-sm");
+    } else {
+      $(".navbar").removeClass("navbar-light bg-light shadow-sm");
     }
+  });
+
+  if ($(".typed-text-output").length == 1) {
+    var typed_strings = $(".typed-text").text();
+    var typed = new Typed(".typed-text-output", {
+      strings: typed_strings.split(", "),
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true,
+    });
+  }
 })(jQuery);
