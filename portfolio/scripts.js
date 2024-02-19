cfd6ba04134165b629e7247346f06728ac24acff
ribeirodavi04 <ribeirodavi04@outlook.com>
2024-02-19 20:13:18 -0300

    feat: adicionar animação no scroll na sessão sobre mim e projetos

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -1,22 +1,38 @@
-(function ($) {
-  "use strict";
-
+$(document).ready(function () {
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 200) {
-      $(".navbar").addClass("navbar-light bg-light shadow-sm");
-    } else {
-      $(".navbar").removeClass("navbar-light bg-light shadow-sm");
-    }
+    $(".navbar").toggleClass("navbar-light bg-light shadow-sm", $(this).scrollTop() > 200);
   });
 
-  if ($(".typed-text-output").length == 1) {
+  //efeito de tecla na section de inicio
+  $(".typed-text-output").each(function () {
     var typed_strings = $(".typed-text").text();
-    var typed = new Typed(".typed-text-output", {
+    new Typed(this, {
       strings: typed_strings.split(", "),
       typeSpeed: 100,
       backSpeed: 20,
       smartBackspace: false,
       loop: true,
     });
-  }
-})(jQuery);
+  });
+
+  //coloca uma animação estilo "fade-in up" "e fade-out down" na section sobre mim e projetos
+  $(window).scroll(function () {
+    
+    $('.animated').each(function () {
+      var sectionAnimated = $(this).offset().top;
+      var windowHeight = $(window).height();
+
+      if ($(window).scrollTop() + windowHeight > sectionAnimated) {
+        $(this).css({
+          opacity: 1,
+          transform: 'translateY(0)'
+        });
+      } else {
+        $(this).css({
+          opacity: 0,
+          transform: 'translateY(100px)'
+        });
+      }
+    });
+  });
+});
